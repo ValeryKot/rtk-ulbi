cace3773d4f676860770019fa642ea8998e2aa32
ValeryKot <kot.valery@gmail.com>
2022-12-27 16:30:25 +0300

    refetch

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import { postAPI } from '../services/PostService'
-import PostItem from './PostItem'
-
+import React, {useEffect, useState} from 'react';
+import {postAPI} from '../services/PostService';
+import PostItem from './PostItem';
 
 const PostContainer = () => {
-    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(5)
+  const [limit, setLimit] = useState(10);
+  const {
+    data: posts,
+    error,
+    isLoading,
+    refetch,
+  } = postAPI.useFetchAllPostsQuery(limit);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLimit(3);
+    }, 2000);
+  }, []);
 
   return (
     <div>
+      <div className='post__list'>
+        <button onClick={() => refetch()}>REFETCH</button>
         {isLoading && <h1>Loading ...</h1>}
         {error && <h1>Error!!!</h1>}
-        <div className='post__list'>{posts && posts?.map(post => <PostItem key={post.id} post={post}/>)}</div>
+        {posts && posts?.map((post) => <PostItem key={post.id} post={post} />)}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostContainer
+export default PostContainer;
