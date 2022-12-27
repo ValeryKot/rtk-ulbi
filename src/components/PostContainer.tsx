fa8eaae6b44ca1cc4b7b1af7b22cb645b1a7be69
ValeryKot <kot.valery@gmail.com>
2022-12-27 16:55:29 +0300

    add posts mutation

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,26 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import {postAPI} from '../services/PostService';
 import PostItem from './PostItem';
+import {IPost} from '../models/IPost';
 
 const PostContainer = () => {
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(100);
   const {
     data: posts,
     error,
     isLoading,
-    refetch,
-  } = postAPI.useFetchAllPostsQuery(limit, {pollingInterval: 1000});
+    // refetch,
+  } = postAPI.useFetchAllPostsQuery(limit);
+
+  const [createPost, {error: createError, isLoading: createLoading}] = postAPI.useCreatePostMutation();
 
   useEffect(() => {
     setTimeout(() => {
       setLimit(3);
-    }, 2000);
+    }, 20000);
   }, []);
+
+  const handleCreate = async () => {
+    const title = prompt();
+    await createPost({title, body: title} as IPost);
+  };
 
   return (
     <div>
       <div className='post__list'>
-        {/* <button onClick={() => refetch()}>REFETCH</button> */}
+        <button onClick={handleCreate}>Add post</button>
         {isLoading && <h1>Loading ...</h1>}
         {error && <h1>Error!!!</h1>}
         {posts && posts?.map((post) => <PostItem key={post.id} post={post} />)}
